9e06ba6688a812af7fd261cf1c9284bd937d16ed
--unset <--unset>
2023-11-01 21:03:49 +0100

    Added a function to open the browser to the correct url

--- MainCodeHandler.py
+++ MainCodeHandler.py
@@ -3,6 +3,7 @@ import datetime as dt
 import os
 import ApiHandler as AH
 import TkinterHandler as TH
+import webbrowser as wb
 # import json as jsn
 # import xmltodict
 # import tkinter as tk
@@ -29,6 +30,10 @@ class Main:
         except Exception as e:
             log.info(e)
             print(e)
+            
+    def browserOpenNitradoTokenPage(self):
+        url = "https://server.nitrado.net/eng/developer/tokens"
+        wb.open_new(url)
 
 
 
