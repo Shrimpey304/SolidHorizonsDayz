c9be56815a37e8defd386b3be82a34948019eceb
SufficientClient <sufficientclient@gmail.com>
2023-11-02 17:29:32 +0100

    continued working on some functions

--- TokenHandler.py
+++ TokenHandler.py
@@ -6,21 +6,34 @@ class TokenHandler():
     
     def tokenFetch(self):
         current_dir = os.path.dirname(__file__)
-        log_file_path = os.path.join(current_dir, 'Content', 'creds.json')
+        log_file_path = os.path.join(current_dir, 'Content', 'Credentials', 'Credentials.json')
         jsonString = open(log_file_path, 'r').read()
-        if(jsonString['encrypted'] == True):
-            print("peeper")
+        return(json.loads(jsonString)['token'], json.loads(jsonString)['encrypted'])
         
     def tokenEncrypt(self, token):
         key = Fernet.generate_key()
         fernet = Fernet(key)
-        encrypted_password = fernet.encrypt(token.encode())
-        return encrypted_password, key
+        encrypted_token = fernet.encrypt(token.encode())
+        return encrypted_token, key
         
     def tokenDecrypt(self, key):
+        current_dir = os.path.dirname(__file__)
+        log_file_path = os.path.join(current_dir, 'Content', 'Credentials', 'Credentials.json')
+        jsonString = open(log_file_path, 'r').read()
+        tokenStringToBytes = bytes(json.loads(jsonString)['token'], 'utf-8')
         fernet = Fernet(key)
-        decrypted_password = fernet.decrypt().decode()
-        return decrypted_password
+        decrypted_token = fernet.decrypt(tokenStringToBytes).decode()
+        return decrypted_token
+    
+    def tokenWrite(self, token, encryption):
+        credentialObj = {
+                "encrypted" : encryption,
+                "token" : token
+        }
+        current_dir = os.path.dirname(__file__)
+        log_file_path = os.path.join(current_dir, 'Content', 'Credentials', 'Credentials.json')
+        jsonFile = open(log_file_path, 'w')
+        jsonFile.write(credentialObj)
     
 instance = TokenHandler()
-instance.tokenFetch()
+print(instance.tokenDecrypt(b'zVdAhjqPJEWqLP1BrsKJXKhpMJZ1G7bE8IYo73SqhBw='))
